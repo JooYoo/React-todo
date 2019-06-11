17057ec581a3595acd297839b6569925e6e76d26
JooYoo <jooyoo@outlook.com>
2019-06-11 16:57:07 +0200

    retrive items works

--- src/App.js
+++ src/App.js
@@ -10,37 +10,50 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
         </header>
         <div className="Todo-container">
-          <input type="text" className="todo-input" placeholder="what needs to be donw" />
+          <input type="text" className="todo-input" placeholder="what needs to be done" />
+
+          {this.state.todos.map((todo, index) =>
+            <div key={todo.id} className = "todo-item">
+              <div className="todo-item-left">
+                <input type="checkbox"/>
+                <div className="todo-item-label">{todo.title}</div>
+              </div>
+              <div className="remove-item">
+                &times;
+              </div>
+            </div>
+          )}
+
         </div>
       </div>
     );
   }
-  
-state={
-  todos:[
-    {
-      'id':1,
-      'title': 'xBox',
-      'completed': false,
-      'editing': false
-    },
-    {
-      'id':2,
-      'title': 'PlayStation',
-      'completed': false,
-      'editing': false
-    },
-    {
-      'id':3,
-      'title': 'Switch',
-      'completed': false,
-      'editing': false
-    }
-  ]
-}
+
+  state = {
+    todos: [
+      {
+        'id': 1,
+        'title': 'xBox',
+        'completed': false,
+        'editing': false
+      },
+      {
+        'id': 2,
+        'title': 'PlayStation',
+        'completed': false,
+        'editing': false
+      },
+      {
+        'id': 3,
+        'title': 'Switch',
+        'completed': false,
+        'editing': false
+      }
+    ]
+  }
 
 }
 
 
 
-  export default App;
+export default App;
