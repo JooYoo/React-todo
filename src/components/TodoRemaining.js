6fb9ffb10500623550ed1e06904a14b47f4ad433
JooYoo <jooyoo@outlook.com>
2019-06-25 14:16:16 +0200

    added stateless functional component

--- src/components/TodoRemaining.js
+++ src/components/TodoRemaining.js
@@ -1,11 +1,23 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-export default class TodoRemaining extends Component {
-    render() {
-        return (
-            <div>
-                {this.props.remaining} items left
-            </div>
-        )
-    }
-}
+// export default class TodoRemaining extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 {this.props.remaining} items left
+//             </div>
+//         )
+//     }
+// }
+
+import React from 'react';
+
+const TodoRemaining = (props) => {
+    return (
+        <div>
+            {props.remaining} items left
+        </div>
+    );
+};
+
+export default TodoRemaining;
